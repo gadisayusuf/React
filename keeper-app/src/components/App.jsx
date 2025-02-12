1755cb4db9494d06fd44cb664f7c612af0c7b546
Gadisa Yusuf Aliyi <gadisa672@gmail.com>
2025-02-12 23:17:13 +0300

    project a note keeper app

---
+++ keeper-app/src/components/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  function createNewNote(newNote) {
+    setNotes(() => {
+      return [...notes, newNote];
+    });
+  }
+  function deleteNote(id) {
+    console.log(id)
+    setNotes((prevNotes)=>{
+        return prevNotes.filter((noteItem,index)=>{
+            return index !== id;
+        })
+    });
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={createNewNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            title={note.title}
+            content={note.content}
+            id={index}
+            key={index}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
